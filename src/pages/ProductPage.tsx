155ceb6241adcd8d2e1b5ca8366d90847ebeba35
Carlxs-Dev <carlosmarrero1510@gmail.com>
2025-09-25 01:15:18 -0400

    Correccion de errores

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,188 +1,174 @@
-import { LuMinus, LuPlus } from "react-icons/lu"
-import { Separator } from "../components/shared/Separator"
-import { formatPrice } from "../helpers"
-import { CiDeliveryTruck } from "react-icons/ci"
-import { Link, useParams } from "react-router-dom"
-import { BsChatLeftText } from "react-icons/bs"
-import { ProductDescription } from "../components/one-product/ProductDescription"
-import { GridImages } from "../components/one-product/GridImages"
-import { useProduct } from "../hooks/products/useProduct"
-import { useEffect, useMemo, useState } from "react"
-import type { VariantProduct } from "../interfaces"
-import { useCounterStore } from "../store/counter.store"
+import { LuMinus, LuPlus } from "react-icons/lu";
+import { Separator } from "../components/shared/Separator";
+import { formatPrice } from "../helpers";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { Link, useParams } from "react-router-dom";
+import { BsChatLeftText } from "react-icons/bs";
+import { ProductDescription } from "../components/one-product/ProductDescription";
+import { GridImages } from "../components/one-product/GridImages";
+import { useProduct } from "../hooks/products/useProduct";
+import { useEffect, useMemo, useState } from "react";
+import type { VariantProduct } from "../interfaces";
+import { useCounterStore } from "../store/counter.store";
 
 interface Acc {
-    [key: string]: {
-        name: string;
-        storages: string[];
-    }
+  [key: string]: {
+    name: string;
+    storages: string[];
+  };
 }
 
 export const ProductPage = () => {
+  const { slug } = useParams<{ slug: string }>();
 
+  const { product, isLoading, isError } = useProduct(slug || "");
 
-    const { slug } = useParams<{ slug: string }>();
+  const [selectedColor, setSelectedColor] = useState<string | null>(null);
+  const [selectedStorage, setSelectedStorage] = useState<string | null>(null);
+  const [selectedVariant, setSelectedVariant] = useState<VariantProduct | null>(null);
 
-    const { product, isLoading, isError } = useProduct(slug || "");
+  const count = useCounterStore(state => state.count);
+  const increment = useCounterStore(state => state.increment);
+  const decrement = useCounterStore(state => state.decrement);
 
-
-    const [selectedColor, setSelectedColor] = useState<string | null>(null)
-
-    const [selectedStorage, setSelectedStorage] = useState<string | null>(null);
-
-    const [selectedVariant, setSelectedVariant] = useState<VariantProduct | null>(null); 
-
-    const count = useCounterStore(state =>state.count);
-    const increment = useCounterStore(state =>state.increment);
-    const decrement = useCounterStore(state =>state.decrement);
-
+  const colors = useMemo(() => {
+    if (!product?.variants) return {};
     
-    const colors = useMemo(() => {
-        return product?.variants.reduce((acc: Acc, variant: VariantProduct) => {
-            const {color, color_name, storage} = variant;
-            if (!acc[color]) {
-                acc[color] = {
-                    name: color_name,
-                    storages: [],
-                };
-            }
-        
+    return product.variants.reduce((acc: Acc, variant: VariantProduct) => {
+      const { color, color_name, storage } = variant;
+      if (!acc[color]) {
+        acc[color] = {
+          name: color_name,
+          storages: [],
+        };
+      }
 
-        if (!acc[color].storages;.includes(storage)){
-            acc[color].storages.push(storage);
-            }   
+      if (!acc[color].storages.includes(storage)) {
+        acc[color].storages.push(storage);
+      }
 
-            return acc;
-        },
-        {} as Acc
-    ) || {};
-    }, [product?.variants]);
+      return acc;
+    }, {} as Acc);
+  }, [product?.variants]);
 
-    const availableColors = Object.keys(colors)
-    useEffect(() => {
-        if (!selectedColor && availableColors.length > 0) {
-            setSelectedColor(availableColors[0]);
-        }
-    }, [availableColors, selectedColor])
+  const availableColors = Object.keys(colors);
 
-    return ( <>
-    <div className="h-fit flex flex-col md:flex-row gap-16 mt-8">
-        <GridImages images={[]} />
+  useEffect(() => {
+    if (!selectedColor && availableColors.length > 0) {
+      setSelectedColor(availableColors[0]);
+    }
+  }, [availableColors, selectedColor]);
+
+  return (
+    <>
+      <div className="h-fit flex flex-col md:flex-row gap-16 mt-8">
+        <GridImages images={product?.images || []} />
 
         <div className="flex-1 space-y-5">
-            <h1 className="text-3xl font-bold tracking-tight">
-                Pan con guayaba y queso
-            </h1>
+          <h1 className="text-3xl font-bold tracking-tight">Pan con guayaba y queso</h1>
 
-            <div className="flex gap-5 items-center">
-                <span className="tracking-wide text-lg font-semibold">
-                    {formatPrice(99999)}
-                </span>
+          <div className="flex gap-5 items-center">
+            <span className="tracking-wide text-lg font-semibold">
+              {formatPrice(99999)}
+            </span>
 
+            <div className="relative">
+              <span>AGOTADO</span>
+            </div>
+          </div>
 
-                <div className="relative">
-                        <span>AGOTADO</span>
+          <Separator />
+
+          <ul className="space-y-2 ml-7 my-10">
+            <li className="text-sm flex items-center gap-2 tracking-tight font-medium">
+              <span className="bg-black w-[5px] h-[5px] rounded-full" />
+              Crema de guayaba de produccion propia
+            </li>
+          </ul>
+
+          <div className="flex flex-col gap-3">
+            <p>Masa fresca</p>
+            <div className="flex gap-3">
+              <button
+                className={`w-8 h-8 rounded-full flex justify-center items-center ${
+                  true ? "border border-slate-800" : ""
+                }`}
+              >
+                <span
+                  className="w-[26px] h-[26px] rounded-full"
+                  style={{ backgroundColor: "blue" }}
+                />
+              </button>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <p className="text-xs font-medium">Opciones disponible</p>
+
+            <div className="flex gap-3">
+              <select className="border border-gray-300 rounded-lg px-3 py-1">
+                <option value="">Solo guayaba y queso</option>
+              </select>
+            </div>
+          </div>
+
+          {false ? (
+            <button
+              className="bg-[#f3f3f3] uppercase font-semibold tracking-widest text-xs py-4 rounded-full transition-all duration-300 hover:bg-[#e2e2e2] w-full"
+              disabled
+            >
+              AGOTADO
+            </button>
+          ) : (
+            <>
+              <div className="space-y-3">
+                <p className="text-sm font-medium">Cantidad:</p>
+
+                <div className="flex gap-8 px-5 py-3 border border-slate-200 w-fit rounded-full">
+                  <button onClick={decrement}>
+                    <LuMinus size={15} />
+                  </button>
+                  <span className="text-sm text-slate-500">{count}</span>
+                  <button onClick={increment}>
+                    <LuPlus size={15} />
+                  </button>
                 </div>
+              </div>
+
+              <div className="flex flex-col gap-3">
+                <button className="bg-[#f3f3f3] uppercase font-semibold tracking-widest text-xs py-4 rounded-full transition-all duration-300 hover:bg-[#e2e2e2]">
+                  Agregar al carro
+                </button>
+                <button className="bg-black text-white uppercase font-semibold tracking-widest text-xs py-4 rounded-full">
+                  Comprar ahora
+                </button>
+              </div>
+            </>
+          )}
+
+          <div className="flex pt-2">
+            <div className="flex flex-col gap-1 flex-1 items-center">
+              <CiDeliveryTruck size={35} />
+              <p className="text-xs font-semibold">
+                Envío gratis para compras mayores a 50$
+              </p>
             </div>
 
-            <Separator />
+            <Link
+              to="#"
+              className="flex flex-col gap-1 flex-1 items-center justify-center"
+            >
+              <BsChatLeftText size={30} />
+              <p className="flex flex-col items-center text-xs">
+                <span className="font-semibold">¿Necesitas ayuda?</span>
+                Contactanos aqui
+              </p>
+            </Link>
+          </div>
+        </div>
+      </div>
 
-
-            <ul className="space-y-2 ml-7 my-10">
-                <li className="text-sm flex items-center gap-2 tracking-tight font-medium">
-                    <span className="bg-black w-[5px] h-[5px] rounded-full"/>
-                    Crema de guayaba de produccion propia
-                </li>
-            </ul>
-
-            <div className="flex flex-col gap-3">
-                <p>
-                    Masa fresca
-                </p>
-                <div className="flex gap-3">
-                    <button className={`w-8 h-8 rounded-full flex justify-center items-center ${
-                        true ? "border border-slate-800" : ""
-                    }`}>
-                        <span 
-                        className="w-[26px] h-[26px] rounded-full"
-                        style={{ backgroundColor: "blue" }}
-                        />
-                    </button>
-                </div>
-            </div>
-
-                    <div className="flex flex-col gap-3">
-                        <p className="text-xs font-medium">
-                            Opciones disponible
-                        </p>
-
-                        <div className="flex gap-3">
-                            <select className="border border-gray-300 rounded-lg px-3 py-1">
-                                <option value="">Solo guayaba y queso</option>
-                            </select>
-                        </div>
-                    </div>
-
-
-                    {
-                        false ?  ( 
-                            <button
-                            className="bg-[#f3f3f3] uppercase font-semibold tracking-widest text-xs py-4 rounded-full transition-all duration-300 hover:bg-[#e2e2e2] w-full"
-                            disabled
-                            >
-                                AGOTADO
-                            </button>
-                        ) : (
-                            <>
-                            <div className="space-y-3">
-                                <p className="text-sm font-medium">
-                                    Cantidad:
-                                </p>
-
-                                <div className="flex gap-8 px-5 py-3 border border-slate-200 w-fit rounded-full">
-                                    <button>
-                                        <LuMinus size={15} />
-                                    </button>
-                                    <span className="text-sm text-slate-500">1</span>
-                                    <button>
-                                        <LuPlus size={15} />
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col gap-3">
-                                <button className="bg-[#f3f3f3] uppercase font-semibold tracking-widest text-xs py-4 rounded-full transition-all duration-300 hover:bg-[#e2e2e2]">
-                                    Agregar al carro
-                                </button>
-                                <button className="bg-black text-white uppercase font-semibold tracking-widest text-xs py-4 rounded-full">
-                                    Comprar ahora
-                                </button>
-                            </div>                        
-                            </>
-                        )}
-
-                        <div className="flex pt-2">
-                            <div className="flex flex-col gap-1 flex-1 items-center">
-                                <CiDeliveryTruck size={35} />
-                                <p className="text-xs font-semibold">
-                                    Envío gratis para compras mayores a 50$
-                                </p>
-                            </div>
-
-                            <Link to="#" className="flex flex-col gap-1 flex-1 items-center justify-center">
-                                <BsChatLeftText size={30} />
-                                <p className="flex flex-col items-center text-xs">
-                                    <span className="font-semibold">
-                                        ¿Necesitas ayuda?
-                                    </span>
-                                    Contactanos aqui
-                                </p>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-
-                <ProductDescription />
+      <ProductDescription />
     </>
-    )
-}
+  );
+};
